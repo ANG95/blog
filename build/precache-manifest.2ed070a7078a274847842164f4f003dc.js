ab2bab153c8d8b8e2be2a94a31894ad94ae0ac57
ang95 <german.apaza.n@gmail.com>
2020-04-22 22:34:12 -0500

    segundo

---
+++ build/precache-manifest.2ed070a7078a274847842164f4f003dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9922e3268510c812a031fc9123ff5602",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dd8005d1f13c1088f9f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5e7b75eb0c3b757fa1e",
+    "url": "/static/js/2.bbeca426.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bbeca426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd8005d1f13c1088f9f",
+    "url": "/static/js/main.83156fff.chunk.js"
+  },
+  {
+    "revision": "f0253b92825acaa4d057",
+    "url": "/static/js/runtime-main.c852e28a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
