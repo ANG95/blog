27203fd57a9754f493484ae91d4cbe9b7d0a8c3a
ang95 <german.apaza.n@gmail.com>
2020-05-24 21:47:43 -0500

    mi primer commit a mi portafolio

---
+++ build/precache-manifest.0cdd495dafcee66c84b5725b461c1fa4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dacc21fd3f9543b4ca8be67d67b3300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91539dbab439a86b2e41",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "69b118f0ac6e242034f0",
+    "url": "/static/css/main.1c599fc5.chunk.css"
+  },
+  {
+    "revision": "91539dbab439a86b2e41",
+    "url": "/static/js/2.236abe3e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.236abe3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b118f0ac6e242034f0",
+    "url": "/static/js/main.21c97d0a.chunk.js"
+  },
+  {
+    "revision": "f0253b92825acaa4d057",
+    "url": "/static/js/runtime-main.c852e28a.js"
+  },
+  {
+    "revision": "d94d63f8317a2cee8c627c3791f5cfcb",
+    "url": "/static/media/imgProgramacion.d94d63f8.jpg"
+  },
+  {
+    "revision": "d2a58c6f6dfafe063f08c24058acd379",
+    "url": "/static/media/yo.d2a58c6f.jpg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Label } from 'reactstrap';
+import yo from "./assets/img/yo.jpg"
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload asi es.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="container-fluid portada">
+        <div className="row">
+          <div className="col-sm-5 text-center">
+            <img src={yo} alt="yo german apaza " className="rounded rounded-circle" width="200em" />
+            <br />
+            <Label
+              style={{
+                fontSize: "4rem",
+                color: "white",
+                fontFamily: "monospace",
+                borderBottom: "1px dashed #189aa2",
+                borderTop: "1px dashed #189aa2",
+              }}>GERMÁN APAZA</Label>
+
+            <p style={{
+              fontSize: "1.5rem",
+              color: "wheat",
+              fontFamily: "courier new"
+            }}>Programador Web Fullstack</p>
+
+            <div style={{ textAlign: "justify",color: "#40fffc" }}>
+              <p>
+                Soy Programador Web Fulltack y Diseñador Gráfico aficionado, estudié la carrera de Ingeniería de Sistemas y me especialicé en aquello que me apasionó, la programación.
+                En los últimos 3 años he desarrollado aplicaciones para escritorio y app moviles con javascript y reactjs asi tambien sitios web que generan valor y aumenta el desempeño de las empresas.
+              </p>
+              <p>
+                Lo que me gusta de hacer esto es ésa sensación gratificante de crear y resolver, principalmente ver la satisfacción del cliente y haberlo apoyado para seguir triunfando,
+                Todo lo que tenga que ver con programación y diseño me interesa. Trato de aprender siempre lo que mas puedo ya que la tecnologia no espera.
+              </p>
+            </div>
+          </div>
+          <div className="col-sm-5 text-center"
+            style={{
+              backgroundColor: "rgba(10, 9, 40, 0.75)",
+              height: "calc(100vh)",
+              boxShadow: "#0a0928 7px -1px 97px 30px"
+            }}>
+            <Label
+              style={{
+                fontSize: "2rem",
+                color: "white",
+                fontFamily: "monospace",
+                borderBottom: "1px dashed #189aa2",
+                borderTop: "1px dashed #189aa2",
+              }}>Mi experiencia</Label>
+          </div>
+          <div className="col-sm-2 d-flex flex-column">
+            {/* <div className=""> */}
+            <button className="botones" style={{ marginTop: "auto" }}>ACERCA DE MI</button>
+            <button className="botones">MI TRABAJO</button>
+            <button className="botones">CONTACTAME</button>
+            <button className="botones">TUTORIALES</button>
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+
+    </Fragment>
   );
 }
 
