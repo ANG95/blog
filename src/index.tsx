12f316fd66904fd52e2bf0405b07d65cb7e1d2f0
GERMAN APAZA <germanapaza@LAPTOP-QDS-EI0128.local>
2025-06-20 14:25:19 -0500

    fix: delete blob

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('No se encontró el elemento root en index.html');
 
-// API de React 18
 const root = createRoot(container as HTMLElement);
 
 root.render(
@@ -16,6 +13,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Para PWA (opcional):
-// serviceWorkerRegistration.register();
